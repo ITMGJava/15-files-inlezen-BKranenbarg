6bae74f0d4f6893bf43290963cbf4df38e9da12e
BKranenbarg <tenazb@gmail.com>
2020-10-29 11:30:52 +0100

    try catch & throws

--- src/FilesInlezen.java
+++ src/FilesInlezen.java
@@ -1,3 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesInlezen {
+    public static void main(String[] args) {
+        try {
+            File f = new File("Hello2.txt");
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                System.out.println(s.nextLine());
+            }
+        } catch (FileNotFoundException f) {
+            System.out.println("Het bestand kan niet worden gelezen");
+            System.out.println("Er is de volgende foutmelding" + f.getMessage());
+        }
+    }
+
+
 }
--- src/FilesInlezen2.java
+++ src/FilesInlezen2.java
@@ -1,4 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FilesInlezen2 {
 
+    public static void main(String[] args) {
+        try {
+            BestandInlezen("Hello2.txt");
+
+        } catch (FileNotFoundException f) {
+            System.out.println("Het bestand kan niet worden gelezen");
+            System.out.println("Er is de volgende foutmelding" + f.getMessage());
+        }
+    }
+
+    static void BestandInlezen(String bestandsNaam) throws FileNotFoundException {
+        File f = new File("Hello2.txt");
+        Scanner s = new Scanner(f);
+        while (s.hasNextLine()) {
+            System.out.println(s.nextLine());
+        }
+
+    }
+
 }
+
+
